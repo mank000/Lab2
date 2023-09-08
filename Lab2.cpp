3a7b086829aa4b9c8da8c8a1a3e7257ca24ef000
mank000 <kozmin_arti@mail.ru>
2023-09-08 21:43:25 +0700

    Первая рабочая версия программы.

--- Lab2.cpp
+++ Lab2.cpp
@@ -1,5 +1,16 @@
 ﻿#include <iostream>
+#include <clocale>
+#define pi 3.14159265358979323846
+using namespace std;
 int main()
 {
-    
+    setlocale(LC_ALL, "ru");
+    while (true) {
+        cout << "Введите число градусов,для выхода из программы напишите -1\n";
+        float UserAngle;
+        cin >> UserAngle;
+        if ((UserAngle >= 0) and (UserAngle < 360)) cout << ((pi * UserAngle) / 180) << "\n";
+        else if (UserAngle == -1) break;
+        else { cout << "Неверный угол.\n"; };
+    }
 }
